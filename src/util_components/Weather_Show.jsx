b083ff9ba1358746caa9b688b4fc005ae946d119
MartinGabco <gabcomartin@gmail.com>
2021-06-07 22:10:20 +0200

    I stored response data from https adress into local state in functional component

--- src/util_components/Weather_Show.jsx
+++ src/util_components/Weather_Show.jsx
@@ -6,7 +6,16 @@ import axios from 'axios';
 const WeatherShow = () => {
     const [latitude, setLatitude] = useState([])
     const [longitude, setLongitude] = useState([])
-    const [weatherItem, setWeatherItem] = useState([])
+
+    const [place, setPlace] = useState([])
+    const [country, setCountry] = useState([])
+    const [situation, setSituation] = useState([])
+    const [temperature, setTemperature] = useState([])  
+    const [temp_min, setTemp_min] = useState([])
+    const [temp_max, setTemp_max] = useState([])
+    const [feels, setFeels] = useState([])   
+    const [pressure, setPressure] = useState([])
+    const [wind, setWind] = useState([])
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(GeolocationPosition => {
@@ -14,16 +23,35 @@ const WeatherShow = () => {
             setLongitude(GeolocationPosition.coords.longitude)
         });
         axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=0797206abd8f52b24a9455dd77220dbc`)
-        .then(responseData => {
-            setWeatherItem(responseData.data.name)
+        .then(response => {
+            setPlace(response.data.name)
+            setCountry(response.data.sys.country)
+
+            const situation = response.data.weather;
+            const loop = situation.map(situation => situation.main)
+            setSituation(loop)
+
+            setTemperature(response.data.main.temp)
+            setTemp_min(response.data.main.temp_min)
+            setTemp_max(response.data.main.temp_max)
+            setPressure(response.data.main.pressure)
+            setFeels(response.data.main.feels_like)
+            setWind(response.data.wind.speed)
         });
-        console.log(latitude)
-        console.log(longitude)
-        console.log(weatherItem)
     });
 
     return (
-        <p>{weatherItem}</p>
+        <React.Fragment>
+            <p><b>{place}</b></p>
+            <p>{country}</p>
+            <p>{situation}</p>            
+            <p>{temperature}</p>
+            <p>{temp_min}</p>
+            <p>{temp_max}</p>
+            <p>{feels}</p>
+            <p>{pressure}</p>
+            <p>{wind}</p>
+        </React.Fragment>
     );
 }
 
