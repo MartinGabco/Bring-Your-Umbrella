8b93a4e986e84b8ac93d447b96e7a825cf0f53bb
MartinGabco <gabcomartin@gmail.com>
2021-06-08 21:33:42 +0200

    Refactoring of WeatherShow component (state holder) functional component in order to catch data through axios.get request to the OpenWeather address. Data from response object I stored in state variables, called according to 3 main themes (general weatherData, currentData and dailyData). I too solved problem of using latitude and longitude constants in HTTP GET method request - by removing them from state and, instead of that step, extracting them from geolocation navigator directly in useEffect() hook.

--- src/util_components/Weather_Show.jsx
+++ src/util_components/Weather_Show.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 // axios
 import axios from 'axios';
@@ -7,56 +7,29 @@ import axios from 'axios';
 import Weather_Item from './Weather_Item';
 
 const WeatherShow = () => {
-    const [latitude, setLatitude] = useState([])
-    const [longitude, setLongitude] = useState([])
-
-    const [place, setPlace] = useState([])
-    const [country, setCountry] = useState([])
-    const [situation, setSituation] = useState([])
-    const [temperature, setTemperature] = useState([])  
-    const [temp_min, setTemp_min] = useState([])
-    const [temp_max, setTemp_max] = useState([])
-    const [feels, setFeels] = useState([])   
-    const [pressure, setPressure] = useState([])
-    const [wind, setWind] = useState([])
+    const [weatherData, setWeatherData] = useState([])
+    const [currentData, setCurrentData] = useState([])
+    const [dailyData, setDailyData] = useState([])
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(GeolocationPosition => {
-            setLatitude(GeolocationPosition.coords.latitude)
-            setLongitude(GeolocationPosition.coords.longitude)
+            const latitude = GeolocationPosition.coords.latitude;
+            const longitude = GeolocationPosition.coords.longitude;
+
+            axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly&appid=0797206abd8f52b24a9455dd77220dbc`)
+            .then(response => {
+                setWeatherData(response.data)
+                setCurrentData(response.data.current);
+                setDailyData(response.data.daily);
+            });
         });
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=0797206abd8f52b24a9455dd77220dbc`)
-        .then(response => {
-            setPlace(response.data.name)
-            setCountry(response.data.sys.country)
+    }, []);
 
-            const situation = response.data.weather.name;
-            setSituation(situation)
-
-            setTemperature(response.data.main.temp)
-            setTemp_min(response.data.main.temp_min)
-            setTemp_max(response.data.main.temp_max)
-            setPressure(response.data.main.pressure)
-            setFeels(response.data.main.feels_like)
-            setWind(response.data.wind.speed)
-
-            console.log(response)
-        });
-    });
+    console.log(weatherData)
 
     return (
         <React.Fragment>
-            <Weather_Item
-                place={place}
-                country={country}
-                situation={situation}
-                temperature={temperature}
-                temp_min={temp_min}
-                temp_max={temp_max}
-                feels={feels}
-                pressure={pressure}
-                wind={wind}
-            />            
+            <Weather_Item currentData={currentData}/>     
         </React.Fragment>
     );
 }
