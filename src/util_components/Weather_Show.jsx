724aceb4da71589201e054200628bc2f12dff050
MartinGabco <gabcomartin@gmail.com>
2021-06-08 23:20:20 +0200

    Added hourData state variable for forecasting weather for next 48 hours in concrete localised place/town. Data, stored in this variable, are coming from specified http address.

--- src/util_components/Weather_Show.jsx
+++ src/util_components/Weather_Show.jsx
@@ -7,8 +7,9 @@ import axios from 'axios';
 import Weather_Item from './Weather_Item';
 
 const WeatherShow = () => {
-    const [weatherData, setWeatherData] = useState([])
+    const [allData, setAllData] = useState([])
     const [currentData, setCurrentData] = useState([])
+    const [hourlyData, setHourlyData] = useState([])
     const [dailyData, setDailyData] = useState([])
 
     useEffect(() => {
@@ -16,16 +17,21 @@ const WeatherShow = () => {
             const latitude = GeolocationPosition.coords.latitude;
             const longitude = GeolocationPosition.coords.longitude;
 
-            axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly&appid=0797206abd8f52b24a9455dd77220dbc`)
+            axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly&lang=sk&appid=0797206abd8f52b24a9455dd77220dbc`)
             .then(response => {
-                setWeatherData(response.data)
+                setAllData(response.data)
                 setCurrentData(response.data.current);
                 setDailyData(response.data.daily);
+            });
+
+            axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=daily&lang=sk&appid=0797206abd8f52b24a9455dd77220dbc`)
+            .then(response => {
+                setHourlyData(response.data.hourly)
             });
         });
     }, []);
 
-    console.log(weatherData)
+    console.log(allData)
 
     return (
         <React.Fragment>
