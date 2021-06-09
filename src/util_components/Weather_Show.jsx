a45af4d5ed76e6c7c462d0cddcc55904eb363d5a
MartinGabco <gabcomartin@gmail.com>
2021-06-09 15:53:55 +0200

    Loader; performing in moments of loading data from external database

--- src/util_components/Weather_Show.jsx
+++ src/util_components/Weather_Show.jsx
@@ -8,16 +8,25 @@ import Current from './Current';
 import Hourly from './Hourly';
 import Daily from './Daily';
 
+// styles
+import '../style/Weather_Show.css';
+
+
 const WeatherShow = () => {
     const [allData, setAllData] = useState([])
     const [currentData, setCurrentData] = useState([])
     const [hourlyData, setHourlyData] = useState([])
     const [dailyData, setDailyData] = useState([])
 
+    // loading
+    const [isLoading, setIsLoading] = useState(false)
+
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(GeolocationPosition => {
             const latitude = GeolocationPosition.coords.latitude;
             const longitude = GeolocationPosition.coords.longitude;
+
+            setIsLoading(true);
 
             axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly&lang=sk&appid=0797206abd8f52b24a9455dd77220dbc`)
             .then(response => {
@@ -30,17 +39,22 @@ const WeatherShow = () => {
             .then(response => {
                 setHourlyData(response.data.hourly)
             });
+
+            setIsLoading(false);
         });
     }, []);
 
     console.log(allData)
 
     return (
-        <React.Fragment>
-            <Current currentData={currentData}/> 
-            <Hourly/>  
-            <Daily/>
-        </React.Fragment>
+        <div className="content">
+            {isLoading && <div className="loader"></div>}
+            {!isLoading && <div className="loaded_content">
+                <Current currentData={currentData}/>
+                <Hourly hourlyData={hourlyData}/>  
+                <Daily dailyData={dailyData}/>
+            </div>}
+        </div>
     );
 }
 
