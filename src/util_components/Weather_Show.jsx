470cbe00dd8740b3e73775eea9b925a84160113c
MartinGabco <gabcomartin@gmail.com>
2021-06-13 10:51:34 +0200

    I created error handling try-catch mechanism, used in case of http errorsoccuring. According to this feature I transformed style of rendering data from state variables.

--- src/util_components/Weather_Show.jsx
+++ src/util_components/Weather_Show.jsx
@@ -11,15 +11,17 @@ import Daily from './Daily';
 // styles
 import '../style/Weather_Show.css';
 
-
 const WeatherShow = () => {
-    const [allData, setAllData] = useState([])
-    const [currentData, setCurrentData] = useState([])
-    const [hourlyData, setHourlyData] = useState([])
-    const [dailyData, setDailyData] = useState([])
+    const [allData, setAllData] = useState([]);
+    const [currentData, setCurrentData] = useState([]);
+    const [hourlyData, setHourlyData] = useState([]);
+    const [dailyData, setDailyData] = useState([]);
 
     // loading
-    const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(false);
+
+    // error handling
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(GeolocationPosition => {
@@ -27,33 +29,50 @@ const WeatherShow = () => {
             const longitude = GeolocationPosition.coords.longitude;
 
             setIsLoading(true);
+            setError(null);
 
-            axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly&lang=sk&appid=0797206abd8f52b24a9455dd77220dbc`)
-            .then(response => {
-                setAllData(response.data)
-                setCurrentData(response.data.current);
-                setDailyData(response.data.daily);
-            });
+            try {
+                const promise1 = axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly&lang=sk&appid=0797206abd8f52b24a9455dd77220dbc`);
+                promise1.then(response => {
+                    setAllData(response.data)
+                    setCurrentData(response.data.current.weather[0]);
+                    setDailyData(response.data.daily);
+                });
+            } catch (ex) {
+                if (ex.response && ex.response.status === 404) {
+                    const message = 'Something went wrong';
+                    setError(message);
+                } else {
+                    const message = 'An unexpected error occurrred!';
+                    setError(message);
+                };
+            };
 
-            axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=daily&lang=sk&appid=0797206abd8f52b24a9455dd77220dbc`)
-            .then(response => {
-                setHourlyData(response.data.hourly)
+            const promise2 = axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=daily&lang=sk&appid=0797206abd8f52b24a9455dd77220dbc`);
+            promise2.then(response => {
+                setHourlyData(response.data.hourly);
             });
 
             setIsLoading(false);
         });
     }, []);
 
-    console.log(allData)
+    // Daily_content
+
+    let daily_content = <div className="loader"></div>;
+
+    if (dailyData.length > 0) {
+        daily_content = <Daily dailyData={dailyData}/>
+    }
+
+    if (error) {
+        daily_content = <p>{error}</p>
+    }
 
     return (
         <div className="content">
-            {isLoading && <div className="loader"></div>}
-            {!isLoading && <div className="loaded_content">
-                <Current currentData={currentData}/>
-                <Hourly hourlyData={hourlyData}/>  
-                <Daily dailyData={dailyData}/>
-            </div>}
+            <p>{daily_content}</p>
+            <p></p>
         </div>
     );
 }
